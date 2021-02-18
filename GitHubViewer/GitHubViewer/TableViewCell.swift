b2727c24993f650c1fa3ce6b9c811b3e2558cc4a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-18 21:00:08 +0900

    UITableViewCellを追加

---
+++ GitHubViewer/GitHubViewer/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  GitHubViewer
+//
+//  Created by home on 2021/02/18.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
