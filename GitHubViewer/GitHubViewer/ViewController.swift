bd21a75ee7d0d73ab5527887a409a35e47abcf96
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-18 21:11:24 +0900

    TableViewにカスタムセルを登録

--- GitHubViewer/GitHubViewer/ViewController.swift
+++ GitHubViewer/GitHubViewer/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
     }
 }
 
