997da76e0ef00f541a844f1ec463299c58704e6a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-25 22:34:12 +0900

    GitHub APIから取得するユーザー情報を定義

--- GitHubViewer/GitHubViewer/GitHub/API/Entity/User.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Entity/User.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    let login: String
+    let id: Int
+    let nodeID: String
+    let avatarURL: URL
+    let gravatarID: String
+    let url: URL
+    let receivedEventsURL: URL
+    let type: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case login, id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case receivedEventsURL = "received_events_url"
+        case type
+    }
+    
+    init(login: String,
+         id: Int,
+         nodeID: String,
+         avatarURL: URL,
+         gravatarID: String,
+         url: URL,
+         receivedEventsURL: URL,
+         type: String) {
+        self.login = login
+        self.id = id
+        self.nodeID = nodeID
+        self.avatarURL = avatarURL
+        self.gravatarID = gravatarID
+        self.url = url
+        self.receivedEventsURL = receivedEventsURL
+        self.type = type
+    }
+}
