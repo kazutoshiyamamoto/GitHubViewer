0ddff6474ea58db488aaec4f6e6756c578d7be5f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-26 22:39:50 +0900

    APIのレスポンスの定義を追加

--- GitHubViewer/GitHubViewer/GitHub/API/Response/ItemsResponse.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Response/ItemsResponse.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct ItemsResponse<Item: Decodable>: Decodable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [Item]
+    
+    private enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case incompleteResults = "incomplete_results"
+        case items
+    }
+    
+    init(totalCount: Int, incompleteResults: Bool, items: [Item]) {
+        self.totalCount = totalCount
+        self.incompleteResults = incompleteResults
+        self.items = items
+    }
+}
