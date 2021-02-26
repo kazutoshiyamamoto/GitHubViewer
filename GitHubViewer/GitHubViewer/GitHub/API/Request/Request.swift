772f708d2518866a57f1239439fa90ed56bacf13
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-26 22:33:47 +0900

    リクエスト処理に関わる変数を定義したプロトコルを追加

--- GitHubViewer/GitHubViewer/GitHub/API/Request/Request.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Request/Request.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+protocol Request {
+    associatedtype Response: Decodable
+    
+    var baseURL: URL { get }
+    var method: HttpMethod { get }
+    var path: String { get }
+    var headerFields: [String: String] { get }
+    var queryParameters: [String: String]? { get }
+}
+
 
 enum HttpMethod: String {
     case get = "GET"
