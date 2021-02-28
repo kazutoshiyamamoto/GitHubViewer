1e504434ee9b46660a6d7b64375d6e4802dcb5e4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-02-28 22:25:44 +0900

    セッションのエラーの種類を定義した列挙型を追加

--- GitHubViewer/GitHubViewer/GitHub/API/Session.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Session.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum SessionError: Error {
+    case noData(HTTPURLResponse)
+    case noResponse
+    case unacceptableStatusCode(Int, Message?)
+    case failedToCreateComponents(URL)
+    case failedToCreateURL(URLComponents)
+}
+
