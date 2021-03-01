8c738e1c9aae91fd1ba82a08adbba9390e11e40f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-01 16:08:56 +0900

    ユーザー検索用のリクエストプロトコルの拡張を追加

--- GitHubViewer/GitHubViewer/GitHub/API/Request/SearchUsersRequest.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Request/SearchUsersRequest.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct SearchUsersRequest: Request {
+    typealias Response = ItemsResponse<User>
+    
+    let method: HttpMethod = .get
+    let path = "/search/users"
+    
+    var queryParameters: [String: String]? {
+        let params: [String: String] = ["q": query]
+        return params
+    }
+    
+    let query: String
+    
+    init(query: String) {
+        self.query = query
+    }
+}
