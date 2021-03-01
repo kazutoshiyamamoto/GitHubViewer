680b6a535dd49b7f50ce046d4d10cb86bed477d6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-01 17:16:10 +0900

    不要な実装を削除

--- GitHubViewer/GitHubViewer/GitHub/API/Entity/User.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Entity/User.swift
@@ -9,39 +9,15 @@ import Foundation
 
 struct User: Codable {
     let login: String
-    let id: Int
-    let nodeID: String
     let avatarURL: URL
-    let gravatarID: String
-    let url: URL
-    let receivedEventsURL: URL
-    let type: String
     
     private enum CodingKeys: String, CodingKey {
-        case login, id
-        case nodeID = "node_id"
+        case login
         case avatarURL = "avatar_url"
-        case gravatarID = "gravatar_id"
-        case url
-        case receivedEventsURL = "received_events_url"
-        case type
     }
     
-    init(login: String,
-         id: Int,
-         nodeID: String,
-         avatarURL: URL,
-         gravatarID: String,
-         url: URL,
-         receivedEventsURL: URL,
-         type: String) {
+    init(login: String, avatarURL: URL) {
         self.login = login
-        self.id = id
-        self.nodeID = nodeID
         self.avatarURL = avatarURL
-        self.gravatarID = gravatarID
-        self.url = url
-        self.receivedEventsURL = receivedEventsURL
-        self.type = type
     }
 }
--- GitHubViewer/GitHubViewer/GitHub/API/Response/ItemsResponse.swift
+++ GitHubViewer/GitHubViewer/GitHub/API/Response/ItemsResponse.swift
@@ -8,19 +8,9 @@
 import Foundation
 
 struct ItemsResponse<Item: Decodable>: Decodable {
-    let totalCount: Int
-    let incompleteResults: Bool
     let items: [Item]
     
-    private enum CodingKeys: String, CodingKey {
-        case totalCount = "total_count"
-        case incompleteResults = "incomplete_results"
-        case items
-    }
-    
-    init(totalCount: Int, incompleteResults: Bool, items: [Item]) {
-        self.totalCount = totalCount
-        self.incompleteResults = incompleteResults
+    init(items: [Item]) {
         self.items = items
     }
 }
