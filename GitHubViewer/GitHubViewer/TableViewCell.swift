53156503a845227c19f30df68bef852435d6bac4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-02 18:27:04 +0900

    セルのユーザー名を扱うプロパティの名称を変更

--- GitHubViewer/GitHubViewer/TableViewCell.swift
+++ GitHubViewer/GitHubViewer/TableViewCell.swift
@@ -8,21 +8,21 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var icon: UIImageView!
-    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var name: UILabel!
     
     private var task: URLSessionTask?
-
+    
     // セルの初期化処理
     override func prepareForReuse() {
         super.prepareForReuse()
-
+        
         task?.cancel()
         task = nil
         imageView?.image = nil
     }
-
+    
     func configure(user: User) {
         // 画像URLからユーザー画像を取得して表示
         task = {
@@ -31,12 +31,12 @@ class TableViewCell: UITableViewCell {
                 guard let imageData = data else {
                     return
                 }
-
+                
                 DispatchQueue.global().async { [weak self] in
                     guard let image = UIImage(data: imageData) else {
                         return
                     }
-
+                    
                     DispatchQueue.main.async {
                         self?.icon?.image = image
                         self?.setNeedsLayout()
@@ -46,7 +46,7 @@ class TableViewCell: UITableViewCell {
             task.resume()
             return task
         }()
-
-        title.text = user.login
+        
+        name.text = user.login
     }
 }
