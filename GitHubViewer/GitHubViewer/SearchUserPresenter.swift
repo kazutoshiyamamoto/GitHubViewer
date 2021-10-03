45300daf2e4dcc3633b75bb8c51957cb267cea72
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-03 20:57:39 +0900

    Presenterの入力に関わるプロトコルを追加

--- GitHubViewer/GitHubViewer/SearchUserPresenter.swift
+++ GitHubViewer/GitHubViewer/SearchUserPresenter.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+protocol SearchUserPresenterInput {
+    var numberOfUsers: Int { get }
+    func user(forRow row: Int) -> User?
+    func didSelectRow(at indexPath: IndexPath)
+    func didTapSearchButton(text: String?)
+}
+
 final class SearchUserPresenter {
     
     
